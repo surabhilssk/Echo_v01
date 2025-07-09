d3eaf100893779c4551872972e56e8ae3e16c8c5
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-07-09 22:49:34 +0530

    Modify prisma to prismaClient

--- lib/db.ts
+++
@@ -1,9 +0,0 @@
-// lib/prisma.ts
-import { PrismaClient } from "@/app/generated/prisma";
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prismaClient =
-  globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prismaClient;
