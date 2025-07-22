25c61bb69417f1de54323f8d4bd92fdf5cdd5d6b
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-07-22 16:35:25 +0530

    Implement visibility play next button

--- app/components/DasboardComponent.tsx
+++ app/components/DasboardComponent.tsx
@@ -6,11 +6,13 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { VideoTile } from "./VideoTile";
 import toast from "react-hot-toast";
+import { useSession } from "next-auth/react";
 
 export const DashboardComponent = ({ creatorId }: { creatorId: string }) => {
   const [streams, setStreams] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [url, setUrl] = useState("");
+  const session = useSession();
 
   async function handleVote(streamId: string) {
     try {
@@ -192,7 +194,14 @@ export const DashboardComponent = ({ creatorId }: { creatorId: string }) => {
           <div>
             <div className="w-full h-64 bg-[#031c26] mt-2 rounded-2xl"></div>
             <div className="mt-3">
-              <ShimmerButton shimmerDuration="4s" className="w-full">
+              <ShimmerButton
+                shimmerDuration="4s"
+                className={`w-full ${
+                  session?.data?.user?.id === creatorId
+                    ? "visible"
+                    : "invisible"
+                }`}
+              >
                 Play Next
               </ShimmerButton>
             </div>
